094c79c496f4e6f29cc69c3aa26d3acbf3dea44e
zsxing99 <44153625+zsxing99@users.noreply.github.com>
2019-10-13 19:06:48 -0700

    security
    remove local host username and password

--- src/db/mysql/MySQLDBUtil.java
+++ src/db/mysql/MySQLDBUtil.java
@@ -2,10 +2,10 @@ package db.mysql;
 
 public class MySQLDBUtil {
 	private static final String HOSTNAME = "localhost";
-	private static final String PORT_NUM = "3306"; // change it to your mysql port number
+	private static final String PORT_NUM = "3306";
 	public static final String DB_NAME = "Jupiter";
-	private static final String USERNAME = "zsxing";
-	private static final String PASSWORD = "S4vKe2N1oTBUuFDl";
+	private static final String USERNAME = ""; // Empty for security
+	private static final String PASSWORD = ""; // Empty for security
 	public static final String URL = "jdbc:mysql://"
 			+ HOSTNAME + ":" + PORT_NUM + "/" + DB_NAME
 			+ "?user=" + USERNAME + "&password=" + PASSWORD
